dae727d3296219578e7b01a130d41d0971ed7c2a
ygorpinto <ygor.pinto@hotmail.com>
2021-10-17 10:49:44 -0300

    ok

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -3,7 +3,14 @@ import LoginStyles from "./styles";
 const Login = () => {
     return (
         <LoginStyles>
-            
+            <div>
+                <h1>Login</h1>
+            </div>
+            <div>
+                <input placeholder="Login"/>
+                <input placeholder="Senha"/>
+                <button>Entrar</button>
+            </div>
         </LoginStyles>
     )
 }
--- src/components/login/styles.js
+++ src/components/login/styles.js
@@ -1,6 +1,31 @@
 import styled from "styled-components";
 
 const LoginStyles = styled.div`
+display: flex;
+flex-direction: column;
+
+div {
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+
+    input {
+        height: 1.5rem;
+        margin: 0.3rem;
+        border-radius: 0.2rem;
+
+        &::placeholder {
+            text-align: center;
+        }
+    }
+
+    button {
+        width: 5rem;
+        height: 2rem;
+    }
+}
 
 `
 
