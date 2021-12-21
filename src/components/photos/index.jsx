f94eb163d9d6347b108b0a69745c14e85afb594a
ygorpinto <ygor.pinto@hotmail.com>
2021-12-20 22:48:01 -0300

    ok

--- src/components/photos/index.jsx
+++ src/components/photos/index.jsx
@@ -3,6 +3,7 @@ import {Redirect} from 'react-router-dom'
 import { useContext } from "react";
 import { GlobalContext } from "../context/globalContext";
 import Cookies from "universal-cookie/es6";
+import api from "../api/api";
 
 const Photos = () => {
 
@@ -13,11 +14,19 @@ const Photos = () => {
 
     const cookies = new Cookies();
 
+    const logout = async () => {
+        cookies.remove('auth_token');
+        window.location.reload();
+    }
+
     return (
         <>
         {(cookies.get('auth_token') && cookies.get('auth_token') !== 'undefined') ? (
             <PhotoStyles>
             <p>VOCE ESTA LOGADO!!</p>
+            <button
+            onClick={logout}
+            >Logout</button>
             </PhotoStyles>
         ) : (<Redirect to="/"/>)}
         </>
